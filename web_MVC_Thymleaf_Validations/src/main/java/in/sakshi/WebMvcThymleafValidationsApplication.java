7732081e6f4172a33900d5719c4eb2cd4aa7fb3d
PrasanthiSakshi <135398157+PrasanthiSakshi@users.noreply.github.com>
2023-08-18 07:16:21 +0530

    Add files via upload

---
+++ web_MVC_Thymleaf_Validations/src/main/java/in/sakshi/WebMvcThymleafValidationsApplication.java
@@ -0,0 +1,13 @@
+package in.sakshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMvcThymleafValidationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMvcThymleafValidationsApplication.class, args);
+	}
+
+}
---
+++ web_MVC_Thymleaf_Validations/src/test/java/in/sakshi/WebMvcThymleafValidationsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sakshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcThymleafValidationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
